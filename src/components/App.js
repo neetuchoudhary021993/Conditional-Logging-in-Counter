3b4dd9fe188caea2af69a5fbd93d9483f373bf4d
neetuchoudhary021993 <132840941+neetuchoudhary021993@users.noreply.github.com>
2023-10-08 10:05:59 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,9 @@ class App extends React.Component{
 
 
   render(){
+    if(this.state.count % 2 === 0) {
     console.log(`Rendering with count:-${this.state.count}`)
+    }
     return(
       <div>
         <span id="count">{this.state.count}</span>
